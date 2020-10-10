7a036b8d73ec3d270bc2e2a11a2656ea4fa6a9ce
MenacingManatee <brett.p.davis101@gmail.com>
2020-10-10 18:41:14 -0500

    Minor error fix

--- models/player.py
+++ models/player.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''module that defines player class'''
-from base_model import Base
+from models.base_model import Base
 
 
 class Player(Base):
     '''class to define a player'''
-    __init__(self):
+    def __init__(self):
         '''method to be called upon object instantiation'''
         self.health = 100
         self.strength = 0
