20274d5da431b9199ebd498cfd798f5b69e56931
MenacingManatee <brett.p.davis101@gmail.com>
2020-10-12 19:34:55 -0500

    Minor changes to save, button and player. Adds character creator screen

--- littlelight.py
+++ littlelight.py
@@ -5,7 +5,7 @@ from models.button import button
 
 def main():
     '''Basic game loop'''
-    global toolsCanvas, gameDisplay, paused
+    global toolsCanvas, gameDisplay, paused, player, save
     paused = True
     toolsCanvas = pygame.Surface((700, 120))
     pygame.init()
@@ -29,6 +29,7 @@ def main():
     newGame = button([width / 3, height / 2 + 50, width / 3, 40], gameDisplay)
     cont = button([width / 3, height / 2 + 100, width / 3, 40], gameDisplay)
     ext = button([width / 3, (height / 2) + 150, width / 3, 40], gameDisplay)
+    save_01 = button([width / 3, height / 2 - 100, width / 3, 40], gameDisplay)
 
     print("w/h: {}/{}".format(width, height))
 
@@ -47,6 +48,9 @@ def main():
             cont.addText('Continue')
             ext.draw()
             ext.addText('Exit')
+        elif name == 'newgame':
+            save_01.draw()
+            save_01.addText('Save 1', -20)
         return 0
 
     def pause_game():
@@ -71,7 +75,88 @@ def main():
             pygame.display.update()
         return False
 
+    def charCreate(save_id, player):
+        '''Allows the user to create a character'''
+        done = False
+        s_points = 6
+        s_points_button = button([width - 250, 10, 200, 30])
+        str_d = button([width - 300, 50, 25, 25])
+        strn = button([width - 250, 50, 150, 25])
+        str_u = button([width - 75, 50, 25, 25])
+        agi_d = button([width - 300, 85, 25, 25])
+        agi = button([width - 250, 85, 150, 25])
+        agi_u = button([width - 75, 85, 25, 25])
+        glo_d = button([width - 300, 120, 25, 25])
+        glo = button([width - 250, 120, 150, 25])
+        glo_u = button([width - 75, 120, 25, 25])
+        d = button([width - 200, height - 30, 175, 25])
+        while not done:
+            gameDisplay.fill((0, 0, 0))
+            s_points_button.draw()
+            s_points_button.addText('Skill Points: ' + str(s_points), 25)
+            str_d.draw()
+            str_d.addText('-')
+            strn.draw()
+            strn.addText('STR: ' + str(player.strength) + '/5', 20)
+            str_u.draw()
+            str_u.addText('+')
+            agi_d.draw()
+            agi_d.addText('-')
+            agi.draw()
+            agi.addText('AGI: ' + str(player.agility) + '/5', 20)
+            agi_u.draw()
+            agi_u.addText('+')
+            glo_d.draw()
+            glo_d.addText('-')
+            glo.draw()
+            glo.addText('GLOW: ' + str(player.glow) + '/5', 20)
+            glo_u.draw()
+            glo_u.addText('+')
+            d.draw()
+            d.addText('Finished', 15)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    if d.rect.collidepoint(pos):
+                        if s_points == 0:
+                            done = True
+                        else:
+                            print('Use all skill points to proceed')
+                    if str_d.rect.collidepoint(pos):
+                        if player.strength > 1:
+                            s_points += 1
+                            player.strength -= 1
+                    elif str_u.rect.collidepoint(pos):
+                        if s_points > 0 and player.strength < 5:
+                            s_points -= 1
+                            player.strength += 1
+                    elif agi_d.rect.collidepoint(pos):
+                        if player.agility > 0:
+                            s_points += 1
+                            player.agility -= 1
+                    elif agi_u.rect.collidepoint(pos):
+                        if s_points > 0 and player.agility < 5:
+                            s_points -= 1
+                            player.agility += 1
+                    elif glo_d.rect.collidepoint(pos):
+                        if player.glow > 1:
+                            s_points += 1
+                            player.glow -= 1
+                    elif glo_u.rect.collidepoint(pos):
+                        if s_points > 0 and player.glow < 5:
+                            s_points -= 1
+                            player.glow += 1
+            pygame.display.flip()
+        save.player = player
+
+
     while not done:
+        ch = displayMenu(menu)
+        if ch == 1:
+            break
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -91,9 +176,19 @@ def main():
                         menu = 'continue'
                     elif newGame.rect.collidepoint(pos):
                         menu = 'newgame'
-        ch = displayMenu(menu)
-        if ch == 1:
-            break
+                elif menu == 'newgame':
+                    if save_01.rect.collidepoint(pos):
+                        print('debug')
+                        from models.player import Player
+                        from models.save import Save
+                        player = Player()
+                        save = Save()
+                        charCreate(save, player)
+                        player.health = player.strength * 5 + player.glow * 2
+                        # startGame(save, player)
+                        print(player.getStats())
+                        pygame.quit()
+                        quit()
         pygame.display.flip()
         clock.tick(20)
 
